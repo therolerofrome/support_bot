791274d015a874d3539e82192cfc15ebb5af4650
snaiper1731 <108128903+snaiper1731@users.noreply.github.com>
2023-06-26 11:21:28 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -14,7 +14,7 @@ session.headers.update({
 
 response = session.get(url)
 print(response)
-print(response.text)
+print(response.json)
 
 
 #получаем инфу о пользователе
@@ -23,4 +23,4 @@ url2 = 'http://external-integration.dev.t1support-portal.dev.ts/api/v1/user'
 response2 = session.get(url2)
 
 print(response2)
-print(response2.text)
+print(response2.json)
